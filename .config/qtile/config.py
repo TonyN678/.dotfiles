9ae8ba83afa511c21f6772298a59e63af408accb
TynoPB <tony.nht16@gmail.com>
2024-04-03 09:52:47 +1100

    clean the code in qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,20 +1,19 @@
 from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, KeyChord, Match, Screen, ScratchPad, DropDown
 from libqtile.lazy import lazy
+from libqtile.scripts.main import VERSION
 from libqtile.utils import guess_terminal
 from qtile_extras import widget
-# from qtile_extras.widget.decorations import BorderDecoration
 from qtile_extras.widget.decorations import RectDecoration
-import os, subprocess
 from libqtile.command import lazy
+import os, subprocess
+
 
 # Run the programs in autostart.sh once after start-up
 @hook.subscribe.startup_once
 def autostart_once():
 	home = os.path.expanduser('~/.config/qtile/autostart.sh')
 	subprocess.Popen([home])
-
-
 
 mod = "mod4" # window key
 alt_key = "mod1"
@@ -25,16 +24,16 @@ terminal = "alacritty"
 keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    
     # Switch between windows
-
     Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-
     Key([mod, "shift"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
@@ -65,50 +64,39 @@ keys = [
         lazy.layout.increase_nmaster(),
         ),
 
-
-    Key([mod], "n", lazy.layout.normalize()),
-     
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key([mod],"f", lazy.window.toggle_fullscreen(),desc="Toggle fullscreen on the focused window",),
-
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    # uncomment to use qtile own search menu
-    # Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     Key([mod], "r", lazy.spawn("rofi -theme ~/.config/rofi/themes/style-2.rasi -show drun")),
-    
     Key([mod], "q", lazy.spawn("./.config/rofi/script/powermenu")),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     
     # Keys to increase/decrease screen brightness
-    # Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl -s set +5%")),
     Key([], "XF86MonBrightnessUp", lazy.spawn("/home/tien/scripts/changebrightness up")),     
-    # Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl -s set 5%-")),
     Key([], "XF86MonBrightnessDown", lazy.spawn("/home/tien/scripts/changebrightness down")),
     
- 
     # Keys to increase/decrease audio volume
-    # Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")),
     Key([], "XF86AudioMute", lazy.spawn("/home/tien/scripts/changevolume mute")),
     Key(["control", alt_key], "slash", lazy.spawn("/home/tien/scripts/changevolume mute")),
-    # Key([], "XF86AudioLowerVolume", lazy.spawn("amixer sset Master 1%-")),
     Key([], "XF86AudioLowerVolume", lazy.spawn("/home/tien/scripts/changevolume down")),
     Key(["control", alt_key], "down", lazy.spawn("/home/tien/scripts/changevolume down")),
-    # Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer sset Master 1%+")),
     Key([], "XF86AudioRaiseVolume", lazy.spawn("/home/tien/scripts/changevolume up")),
     Key(["control", alt_key], "up", lazy.spawn("/home/tien/scripts/changevolume up")),
 	
+    # Keys for toggling music player with headphones
     Key([], "XF86AudioPause", lazy.spawn("/usr/bin/mocp -G")),
     Key([], "XF86AudioPlay", lazy.spawn("/usr/bin/mocp -G")),
     Key([], "XF86AudioNext", lazy.spawn("/usr/bin/mocp -f")),
     Key([], "XF86AudioPrev", lazy.spawn("/usr/bin/mocp -r")),
     
-    # For taking screenshot with selected area with scrot
-    Key([mod], "p", lazy.spawn("/usr/bin/scrot --select")),
     # For changing audio output device
     Key([mod], "a", lazy.spawn("/home/tien/scripts/audio-output")),
+    
+    # For taking screenshot with selected area with scrot
+    Key([mod], "p", lazy.spawn("/usr/bin/scrot --select")),
 
 ]
 
@@ -118,14 +106,9 @@ keys = [
 # a list contains Group objects represent each workspaces in the groupbox widget 
 groups = []
 
-# Group's number
+# Group's info
 group_names = ["1", "2", "3", "4",]
-
-#group_labels = ["1 ", "2 ", "3 ", "4 ", "5 ", "6 ",]
-#group_labels = ["", "", "", "", "", "",] 
 group_labels = ["󰏃","󰏃","󰏃","󰏃"] 
-#group_labels = ["Terminal", "CodeResources", "Web", "Music", "Files", "Documents", ]
-
 group_layouts = [ "columns", "columns", "columns", "columns"]
 
 # Create Group objects with attributes and paste to the list called groups
@@ -149,7 +132,6 @@ for i in groups:
         Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
         # MOVE WINDOW TO SELECTED WORKSPACE 1-10 AND FOLLOW MOVED WINDOW TO WORKSPACE
         Key([mod, "control"], i.name, lazy.window.togroup(i.name) , lazy.group[i.name].toscreen()),
-
         ]
     )
 
@@ -167,11 +149,13 @@ keys.extend([
     Key([mod, "shift"], "Return", lazy.group['scratchpad'].dropdown_toggle('term')),
     Key([mod], "m", lazy.group['scratchpad'].dropdown_toggle('music')),
     Key([mod], "v", lazy.group['scratchpad'].dropdown_toggle('protonvpn')),
-    #lazy.spawn("st gnome-keyring-daemon --start"), lazy.spawn("st nm-applet"),  
-    # Key([mod], "b", lazy.group['scratchpad'].dropdown_toggle('weather')),
 ])
 
-#------------ FLOATING WINDOW ----------------- #
+
+#--------------------- FLOATING WINDOW ------------------------- #
+
+#Reference:
+# https://docs.qtile.org/en/latest/manual/commands/api/windows.html
 
 # reize foating windows with keybinds
 @lazy.window.function 
@@ -185,12 +169,17 @@ def move_floating_window(window, x: int = 0, y: int = 0):
     new_y = window.float_y + y
     window.cmd_set_position_floating(new_x, new_y)
 
-# turn a window to floating state, move to middle and resize
+# turn a window to floating state, move to middle and resize, else tile the window
 @lazy.window.function 
-def zoom_in_window(window):
-    lazy.window.toggle_floating()
-    window.cmd_center()
-    window.cmd_set_size_floating(790, 600)
+def toggle_window(window):
+    state = window.info()
+    if state['floating'] == True:
+        window.disable_floating()
+    else:
+        window.enable_floating()
+        window.set_size_floating(750, 700)
+        window.cmd_center()
+        window.keep_above()
 
 keys.extend([
 Key([mod, "control" ], "h", resize_floating_window(width=10), desc='increase width by 10'), 
@@ -203,8 +192,7 @@ Key([mod, 'shift'], 'l', move_floating_window(x=-10), desc='move 10 right'),
 Key([mod, 'shift'], 'k', move_floating_window(y=10), desc='move 10 up'),
 Key([mod, 'shift'], 'j', move_floating_window(y=-10), desc='move 10 down'),
 
-Key([mod, 'shift'], 't', zoom_in_window(), desc='immidiately turn a window to floating, move to middle of the screen and resize'),
-Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+Key([mod], 't', toggle_window(), desc='immidiately turn a window to floating, move to middle of the screen and resize, else if is float, tile it'),
 
 ])
 
@@ -212,20 +200,19 @@ Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focu
 """"""""""""""""""
 """" LAYOUTS """""
 """"""""""""""""""
+#https://docs.qtile.org/en/latest/manual/ref/layouts.html
 
 def init_layout_theme():
 	return {
 		"border_focus": '#b3b1f6',
-		 "border_normal": '#00000000',
+		"border_normal": '#000000',
 		}
 
 layout_theme = init_layout_theme()
 
 layouts = [
     layout.MonadTall(**layout_theme, allign=0, border_width=2, change_size=10, margin=8, single_margin=10, single_border_width=0),
-    layout.Columns(**layout_theme,allign=0, border_on_single=True, border_width=2, single_border_width=2,  margin=8, margin_on_single=10),
-    layout.Zoomy(**layout_theme)
-    #layout.Max(),
+    layout.Columns(**layout_theme,allign=0, border_on_single=True, border_width=2, single_border_width=1,  margin=8, margin_on_single=10),
 ]
 
 """"""""""""""""""
@@ -254,7 +241,7 @@ def init_colors():
         ["#a265e5", "#a265e5"],  #16  purple
         ["#ecb4e2", "#ecb4e2"], #17 pink bubblegum
         ["#afeeff", "#afeeff"], #18 light cyan
-	["#6f7787", "#6f7787"], #19 light grey
+	    ["#6f7787", "#6f7787"], #19 light grey
         ["#CAA9E0", "#CAA9E0"], #20 purple-pink"
     ]
 
@@ -266,7 +253,7 @@ screens = [
         top=bar.Bar(
             [
                 widget.Spacer(
-                    length = 20
+                    length = 10
                     ),
 
                # widget.TextBox(
@@ -303,9 +290,9 @@ screens = [
                     disable_drag = False,
                     fontsize = 16,
                     spacing=5,
-		    margin = 10,
-		    urgent_alert_method="line",
-		    urgent_border=colors[15],
+		            margin = 10,
+		            urgent_alert_method="line",
+		            urgent_border=colors[15],
                     decorations = [RectDecoration(colour=colors[0], radius=13, filled=True, padding_y=6)]
                     ),
 
@@ -314,16 +301,14 @@ screens = [
                         ),
 
                widget.Clock(
-                        font='FiraCode Nerd Font Propo',
-                        format="  %d/%m/%y | 󱎫 %H:%M ",
-                        foreground=colors[20],
-                        padding=0,
-                        fontsize=16,
-			mouse_callbacks = {'Button1': lazy.group['scratchpad'].dropdown_toggle('Calendar') },
-                        decorations=[
-                            RectDecoration(colour=colors[0], radius=13, filled=True, padding_y=6)
-                        ]
-
+                   font='FiraCode Nerd Font Propo',
+                   format="  %d/%m/%y | 󱎫 %H:%M ",
+                   foreground=colors[20],
+                   padding=0,
+                   fontsize=16,
+			       mouse_callbacks = {'Button1': lazy.group['scratchpad'].dropdown_toggle('Calendar') },
+                   decorations=[
+                       RectDecoration(colour=colors[0], radius=13, filled=True, padding_y=6)]
                 ), 
 		 
                 widget.Spacer(
@@ -339,63 +324,9 @@ screens = [
                     decorations=[
                         RectDecoration(colour=colors[0], radius=13, filled=True, padding_y=6, padding_x=9)]
                     ),
+                
                 widget.Spacer(
-                    length = 2
-                    ),
-
-#		widget.CurrentLayoutIcon(
-#			foreground=colors[20],
-#			scale= 0.45,
-#			fmt = " {} ",
-#			padding= 15,
-#			decorations = [RectDecoration(colour=colors[0], extra_width=20,radius=13, filled=True, padding_y=6)]
-#			
-#		   ),
-
-                widget.Spacer(
-                    length = 750,
-                        ),
-
-
-
-	#	widget.TextBox(
- 	#		text="  󰂯 ",
-	#		foreground = colors[20],
-	#		mouse_callbacks = {'Button1': lazy.spawn('./.config/rofi/script/bluetooth')},
- 
-        #                decorations =[
-        #                    RectDecoration(colour=colors[0], group=True,  radius=13, filled=True, padding_y=6)
-	#		    ]
-	#		),
-        #        widget.TextBox(
- 	#		text="󰖩  ",
-	#		foreground = colors[20],
-	#		mouse_callbacks = {'Button1': lazy.spawn('./.config/rofi/script/wifimenu')},
-        #                decorations =[
-        #                    RectDecoration(colour=colors[0], group=True,  radius=13, filled=True, padding_y=6)
-        #                    ]
-        #                ),
-
-#		widget.Wttr(
-#			font = "FiraCode Nerd Font",
-#			fontsize = 16,
-#			foreground = colors[20],
-#			padding = 15,
-#			location = {},
-#			# Add %l for location, 
-#			format = "%c%t | %w",
-#			units = 'm',
-#			update_interval = 10,
-#			user_agent = 'Qtile',
-#			mouse_callbacks = {'Button1': lazy.group['scratchpad'].dropdown_toggle('weather') },
-#                        decorations =[
-#                            RectDecoration(colour=colors[0], radius=13, filled=True, padding_y=6)
-#                            ]
-#			
-#			),
-		
-                widget.Spacer(
-                        length = 10,
+                    length = 770,
                         ),
 
                 widget.Battery(
@@ -431,7 +362,7 @@ screens = [
                     format='  {MemUsed: .1f}{mm} /{MemTotal: .0f}{mm}  ',
                     fontsize=16,
                     measure_mem='G',
- 		    mouse_callbacks = {'Button1': lazy.group['scratchpad'].dropdown_toggle('htop')},
+ 		            mouse_callbacks = {'Button1': lazy.group['scratchpad'].dropdown_toggle('htop')},
                     decorations=[
                         RectDecoration(colour=colors[0], radius=13, filled=True, padding_y=6)
                         ]
@@ -459,15 +390,13 @@ screens = [
                 widget.Spacer(
                         length = 10,
                         ),
-
+   	            
+                # if the widget stop working someday, check the backlight_name again at /sys/class/backlight/
                 widget.Backlight(
                     font="FiraCode Nerd Font Propo",
                     fontsize=16,
                     format = ' 󱩒 {percent:2.0%} ',
                     foreground=colors[20],
-		   
-   	            # if the widget stop working someday, check the name again at /sys/class/backlight/
-
                     backlight_name='amdgpu_bl0',
                     #backlight_name='amdgpu_bl1',
                     decorations=[
@@ -488,12 +417,13 @@ screens = [
                         fmt='  {} ',
                         fontsize=16,
                         scroll=True,
-			mouse_callbacks={'Button1': lazy.spawn("pavucontrol"), 'Button3': lazy.group['scratchpad'].dropdown_toggle('Visualiser') },
-                         decorations=[
+			            mouse_callbacks={'Button1': lazy.spawn("pavucontrol"), 'Button3': lazy.group['scratchpad'].dropdown_toggle('Visualiser') },
+                        decorations=[
                             RectDecoration(colour=colors[0], radius=13, filled=True, padding_y=6,group=True)
                         ]
 
                 ),                
+                
                 widget.Volume(
                         font="FiraCode Nerd Font Propo",
                         foreground=colors[20],
@@ -501,7 +431,7 @@ screens = [
                         fmt='{}  ',
                         fontsize=16,
                         scroll=True,
-			mouse_callbacks={'Button1': lazy.spawn("pavucontrol"), 'Button3': lazy.group['scratchpad'].dropdown_toggle('Visualiser') },
+			            mouse_callbacks={'Button1': lazy.spawn("pavucontrol"), 'Button3': lazy.group['scratchpad'].dropdown_toggle('Visualiser') },
                          decorations=[
                             RectDecoration(colour=colors[0], radius=13, filled=True, padding_y=6,group=True)
                         ]
@@ -509,28 +439,23 @@ screens = [
 
 
                widget.Spacer(
-		length = 5,	
-		),
+		                length = 5,	
+		                ),
  
-		widget.Systray(
-			icon_size = 25,
-			padding = 2,
-		),
+        		widget.Systray(
+		            	icon_size = 25,
+			            padding = 5,
+		                ),
         
-#        widget.StatusNotifier(
-#			icon_size = 15,
-#			padding = 2,
-#            decorations=[
-#                        RectDecoration(colour=colors[0], radius=13, filled=True, padding_y=6,group=False)
-#                    ]
-#        ),
-
-
             ],
+            # the size of bar
             45,
+            # opacity of bar
             opacity=1.0,
+            # background color of bar --> transparent
             background=["#00000000"],
-            # north east south west
+
+            # Direction: north east south west
 	        margin = [0,0,-7,0],
             border_width=[0,0,0,0],  # Draw top and bottom borders
             # border_color=["f0f0ef", "bae1ff", "f0f0ef", "bae1ff"]  # Borders are magenta
@@ -539,7 +464,7 @@ screens = [
     ),
 ]
 
-# Drag floating layouts.
+# Drag floating window with mouse and mod key
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
@@ -602,4 +527,4 @@ wl_input_rules = None
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
-wmname = "LG3D"
+wmname = f"Qtile {VERSION}"
